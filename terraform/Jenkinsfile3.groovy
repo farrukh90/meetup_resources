0234eeed6002197f6d1c3f2d77e42deb9657c95a
farrukh90 <farrukhsadykov@gmail.com>
2019-05-06 23:06:10 -0500

    folder changed

--- terraform/Jenkinsfile3.groovy
+++ terraform/Jenkinsfile3.groovy
@@ -3,7 +3,7 @@ node {
         git 'https://github.com/farrukh90/meetup_resources.git'
     }
     stage("Initialization"){
-        customWorkspace '${workspace}/terraform'
+        dir('${workspace}/terraform')
             sh "terraform get"
             sh "terraform init"
     }
