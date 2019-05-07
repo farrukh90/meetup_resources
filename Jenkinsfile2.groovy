22cec4170960aa527427adc0a32d82c3509b1ad1
farrukh90 <farrukhsadykov@gmail.com>
2019-05-06 21:35:55 -0500

    added flaskapp

--- Jenkinsfile2.groovy
+++ Jenkinsfile2.groovy
@@ -1,4 +1,8 @@
 node{
     stage("Repo Clone"){
+        git 'git@github.com:farrukh90/meetup_resources.git'
+    }
+    stage("Build Image"){
+        sh "packer build -var-file=values.json flaskapp.json"
     }
 }
