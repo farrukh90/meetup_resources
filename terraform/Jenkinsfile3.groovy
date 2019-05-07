4cfeaaf49d870bdb5d47cbf2d903e311045392c5
farrukh90 <farrukhsadykov@gmail.com>
2019-05-06 23:13:54 -0500

    **

--- terraform/Jenkinsfile3.groovy
+++ terraform/Jenkinsfile3.groovy
@@ -2,9 +2,8 @@ node {
     stage("Clone a repo"){
         git 'https://github.com/farrukh90/meetup_resources.git'
     }
-    ws("${workspace}/terraform/"){
-        stage("Initialization"){
-            sh "pwd"
+    stage("Initialization"){
+        sh "cd terraform && terraform get"
     }
     stage("Terraform Plan"){
         sh "terraform plan -var-file=instance.tfvars"
