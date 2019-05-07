7672d5f844d33c8cd9e3d7f947438098d57afdde
farrukh90 <farrukhsadykov@gmail.com>
2019-05-06 23:02:55 -0500

    workspace changed

--- terraform/Jenkinsfile3.groovy
+++ terraform/Jenkinsfile3.groovy
@@ -3,8 +3,9 @@ node {
         git 'https://github.com/farrukh90/meetup_resources.git'
     }
     stage("Initialization"){
-        sh "terraform get"
-        sh "terraform init"
+        customWorkspace '${workspace}/terraform'
+            sh "terraform get"
+            sh "terraform init"
     }
     stage("Terraform Plan"){
         sh "terraform plan -var-file=instance.tfvars"
