fd67d2884c4fc702b7dbecdcb3ec08025a990cbe
farrukh90 <farrukhsadykov@gmail.com>
2019-05-06 23:10:42 -0500

    workspace added

--- terraform/Jenkinsfile3.groovy
+++ terraform/Jenkinsfile3.groovy
@@ -3,7 +3,7 @@ node {
         git 'https://github.com/farrukh90/meetup_resources.git'
     }
     stage("Initialization"){
-        ws ("terraform/"){
+        ws ("${workspace}/terraform/"){
             sh "pwd"
     }
     stage("Terraform Plan"){
